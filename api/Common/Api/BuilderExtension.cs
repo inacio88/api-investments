fe36003b7b465158ad4ea6cf3c9deeda2c52de6c
inacio88 <inacio88@outlook.com>
2025-11-26 22:59:27 -0300

    feat: configuracoes basica da api e identity  development

---
+++ api/Common/Api/BuilderExtension.cs
@@ -0,0 +1,76 @@
+using api.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Common.Api
+{
+    public static class BuilderExtension
+    {
+        public static void AddConfiguration(this WebApplicationBuilder builder)
+        {
+            Configuration.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                                            ?? string.Empty;
+
+        }
+
+        public static void AddDocumentation(this WebApplicationBuilder builder)
+        {
+
+            // builder.Services.AddEndpointsApiExplorer();
+            // builder.Services.AddSwaggerGen(x =>
+            // {
+            //     x.CustomSchemaIds(x => x.FullName);
+            // });
+
+            
+        }
+
+        public static void AddSecurity(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddIdentityApiEndpoints<IdentityUser>();
+                            //.AddEntityFrameworkStores<ApplicationDbContext>();
+
+            // builder.Services.Configure<IdentityOptions>(options =>
+            // {
+            //     options.SignIn.RequireConfirmedEmail = true;
+            //     options.Lockout.MaxFailedAccessAttempts = 20;
+
+            // });
+
+            // builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
+            //                 .AddIdentityCookies();
+            // builder.Services.ConfigureApplicationCookie(options =>
+            // {
+            //     options.Cookie.Name = "Investment.AuthCookie";
+            //     options.Cookie.HttpOnly = true;
+            //     options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            //     options.Cookie.SameSite = SameSiteMode.None;
+            //     options.Cookie.IsEssential = true;
+            //     options.ExpireTimeSpan = TimeSpan.FromDays(7);
+            //     options.SlidingExpiration = true;
+            // });
+
+            builder.Services.AddAuthorization();
+        }
+
+        public static void AddDataContexts(this WebApplicationBuilder builder)
+        {
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseNpgsql(Configuration.ConnectionString);
+            });
+
+            builder.Services.AddIdentityCore<IdentityUser>()
+                            .AddEntityFrameworkStores<ApplicationDbContext>()
+                            .AddApiEndpoints()
+                            ;
+        }
+
+
+        public static void AddServices(this WebApplicationBuilder builder)
+        {
+
+        }
+    }
+}
---
+++ api/Common/Api/Configuration.cs
@@ -0,0 +1,8 @@
+namespace api.Common.Api
+{
+   public static class Configuration
+    {
+        public static string ConnectionString { get; set; } = string.Empty;
+
+    }
+}
