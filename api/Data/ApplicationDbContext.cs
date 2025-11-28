deb352efca46ab670b8d2439bf4cabb297aeb09a
inacio88 <inacio88@outlook.com>
2025-11-28 00:09:40 -0300

    feat: projeto infra  development

--- api/Data/ApplicationDbContext.cs
+++ api/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using core.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +8,8 @@ namespace api.Data
 {
     public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
+        public DbSet<Investment> Investments { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
             base(options)
         { }
