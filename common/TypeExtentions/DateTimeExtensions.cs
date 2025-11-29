d80fbf9ff8aeea8403760919c180eae694f61893
inacio88 <inacio88@outlook.com>
2025-11-29 14:02:39 -0300

    feat: extensoes datetime  development

---
+++ common/TypeExtentions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace common.TypeExtentions
+{
+    public static class DateTimeExtensions
+    {
+        public static bool IsDateGreaterThan(this DateTime leftSide, DateTime rightSide)
+        {
+            return leftSide.Date > rightSide.Date;
+        }
+
+        public static bool IsDateLessThan(this DateTime leftSide, DateTime rightSide)
+        {
+            return leftSide.Date > rightSide.Date;
+        }
+    }
+}
